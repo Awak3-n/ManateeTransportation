966157dbd445cf02193a74e8ff6aa2453e45f5af
Mkomar2021 <michael.richard.komar@gmail.com>
2023-10-13 12:20:49 -0400

    the code works now.

--- manateeTransport.go
+++ manateeTransport.go
@@ -16,22 +16,20 @@ a seprate function uses the number as a set of instructions. In the binary numbe
 0 represents a manatee sent into the left tub, and 1 repesents a manatee sent into the right tub.
 it then compares each of the maximums from all of the given binary combinations and returns the highest maximum
 */
-func getMaxManatees(manateeList *list.List, tubLength int) int {
+func getMaxManatees(manateeList *list.List, tubLength int) list.List {
 	len := manateeList.Len()                    // amount of manatees stored in list
 	possibleCombos := math.Pow(2, float64(len)) // maximum amount of possible combinations with inputting manatees into the left or right tub (2^n)
-	var localMax int
-	var globalMax int
+	var tempTub list.List
+	var outputTub list.List
 
 	for i := 0; i < int(possibleCombos); i++ {
 		currentCombo := PadLeft(strconv.FormatInt(int64(i), 2), len) // currentCombo is a padded binary number of type string, aka the instructions that getLocalMaxManatees will follow
-		localMax = getLocalMaxManatees(manateeList, tubLength, currentCombo)
-
-		if localMax > globalMax {
-			globalMax = localMax
+		tempTub = getLocalMaxManatees(manateeList, tubLength, currentCombo, outputTub)
+		if tempTub.Len() > outputTub.Len() {
+			outputTub = tempTub
 		}
 	}
-
-	return globalMax
+	return outputTub
 }
 
 // pads string with 0 from the left side
@@ -49,12 +47,14 @@ func PadLeft(binaryStr string, length int) string {
 getLocalMaxManatees calculates the amount of manatees that can fit based of a binary number that getMaxManatees supplies it with
 it then returns the maximum amount of manatees it can fit into the tube based on the provided instructions
 */
-func getLocalMaxManatees(manateeQueue *list.List, tublength int, strInstructions string) int {
+func getLocalMaxManatees(manateeQueue *list.List, tublength int, strInstructions string, outputTub list.List) list.List {
 	var localMax, leftTub, rightTub int
+	tempTub := list.New()
 
 	// base case #1: no manatees :(
 	if manateeQueue.Front() == nil {
-		return localMax
+		//return localMax
+		return *tempTub
 	}
 
 	// i know im only pushing ints into the list, so I be casting
@@ -62,17 +62,24 @@ func getLocalMaxManatees(manateeQueue *list.List, tublength int, strInstructions
 	currentIntManatee := currentManatee.Value.(int)
 
 	for i := 0; i < len(strInstructions); i++ {
-
 		// puts currentIntManatee into lefttub when the current instruction is a 0
-		if string(strInstructions[i]) == "0" && currentIntManatee+leftTub <= tublength {
+		if currentIntManatee+leftTub <= tublength && string(strInstructions[i]) == "0" {
+			strPort := "port"
+			tempTub.PushBack(strPort)
 			leftTub += currentIntManatee
 			localMax++
+		} else if string(strInstructions[i]) == "0" {
+			return *tempTub
 		}
 
 		// puts currentIntManatee into righttub when the current instruction is a 1
-		if string(strInstructions[i]) == "1" && currentIntManatee+rightTub <= tublength {
+		if currentIntManatee+rightTub <= tublength && string(strInstructions[i]) == "1" {
+			strStarboard := "starboard"
+			tempTub.PushBack(strStarboard)
 			rightTub += currentIntManatee
 			localMax++
+		} else if string(strInstructions[i]) == "1" {
+			return *tempTub
 		}
 
 		// base case #2: no more manatees to add to the tubs
@@ -80,17 +87,20 @@ func getLocalMaxManatees(manateeQueue *list.List, tublength int, strInstructions
 			currentManatee = currentManatee.Next()
 			currentIntManatee = currentManatee.Value.(int)
 		} else {
-			return localMax
+			return *tempTub
 		}
 
-		//base case #3: tubs are full
+		//base case #3: both tubs are full
 		if currentIntManatee+leftTub > tublength && currentIntManatee+rightTub > tublength {
-			return localMax
+			return *tempTub
 		}
+
+		// if instruction = 0 and the if statement for appending port was not executed,
+
 	}
 
 	// returns error if the base cases do not return
-	return -1
+	return *list.New().Init()
 }
 
 //func addPort() {
@@ -112,7 +122,7 @@ func main() {
 	if err == nil {
 		if tubLength >= 1 && tubLength <= 100 {
 			tubLength *= 100
-			fmt.Printf("tublength: %d\n", tubLength)
+			//fmt.Printf("tublength: %d\n", tubLength)
 		}
 	}
 
@@ -123,20 +133,19 @@ func main() {
 			if manatee != 0 {
 				if manatee >= 100 && manatee <= 3000 {
 					manateeQueue.PushBack(manatee)
-					fmt.Printf("pushing manatee: %d\n", manatee)
+					//fmt.Printf("pushing manatee: %d\n", manatee)
 				}
 			} else {
 				break
 			}
 		}
 	}
+	output := getMaxManatees(manateeQueue, tubLength) // maximum amount of possible combinations of manatees
+	max := output.Len()
+	fmt.Printf("%d\n", max)
+	//fmt.Printf("tublength: %d\n", tubLength)
 
-	max := getMaxManatees(manateeQueue, tubLength) // maximum amount of possible combinations of manatees
-
-	fmt.Printf("max amount of manatees: %d\n", max)
-	fmt.Printf("tublength: %d\n", tubLength)
-
-	for i := manateeQueue.Front(); i != nil; i = i.Next() {
+	for i := output.Front(); i != nil; i = i.Next() {
 		fmt.Println(i.Value)
 	}
 }
